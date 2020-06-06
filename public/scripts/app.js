ce1b80ce74554894b6894e8e8af875776d8ae86f
Sean Stoneburner <seanstoneburner@Seans-MacBook-Pro.local>
2020-06-06 15:15:18 -0400

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('app.js is running');
+
+var template = React.createElement(
+  'p',
+  null,
+  'Indecision app'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('app.js is running');
+
+var template = <p>Indecision app</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
